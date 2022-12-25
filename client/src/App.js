064593543dc6124009a2d0a7706e96780df3e1a0
shubh asati <shubhtrikal9876@gmail.com>
2022-12-26 00:48:10 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import {Route ,Routes} from 'react-router-dom';
+import Homepage from './pages/homepage';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ client/src/pages/homepage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// import {auth} from 'firebase/app'
+
+const Homepage = () => {
+    
+    return (
+        <div>
+            <h1>Homepage</h1>
+        </div>
+    )
+}
+
+export default Homepage
